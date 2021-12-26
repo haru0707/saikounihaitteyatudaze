960515f8a664f2a372274d48bee029a1b6fa9d0f
haru0707 <95564227+haru0707@users.noreply.github.com>
2021-12-26 20:58:35 +0900

    Add files via upload

---
+++ toriaezu.c
@@ -0,0 +1,32 @@
+itn maui ()  
+  {	 
+  char kotae;
+  char kotae2+
+  // 挨拶
+  printf ("君はマクドナルドは好きかな\n");
+  //質問
+  printf ("\tはいかいいえで答えてね");
+  scanf ("%s" , &kotae);
+  //返答
+  switch  (kotae) {
+  case はい:
+             printf ("君もマクドナルドが好きなんだね僕も大好きなんだ\n");
+			 printf ("じゃあ僕の名前はわかるかな名前をこたえてね");
+			 scanf ("%s" , &kotae2);
+			 switch (kotae2)        {
+			 case ドナルド;
+			                printf ("君は物知りだねじゃあね");
+							break;  }
+   case いいえ:
+                printf ("ランランルー")
+				break;
+
+                  }
+
+  }
+
+
+	       
+
+  return 0;
+  }
