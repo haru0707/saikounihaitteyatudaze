6cbb223f6ac0dcf8628c0506f76187ef21c76af1
Bellomaker <88414763+Bellomaker@users.noreply.github.com>
2021-12-26 21:53:13 +0900

    Update ChatApp.php

--- ChatApp.php
+++ ChatApp.php
@@ -3,12 +3,44 @@
 class ChatApp {
   public $title;
   public $script;
-  public $file;
+  public $path;
   // アプリケーションの設定[タイトル、説明、アプリを保存するファイル]
-  function __construct($title, $script, $file){
+  // 既にアプリを保存するファイルはあるものとしていますので、実在するファイルパスをお願いします。拡張子は[.json]が好ましいです。
+  function __construct($title, $script, $path){
     $this->title = $title;
     $this->script = $script;
-    $this->file = $file;
+    $this->path = realpath($path);
+  }
+  function buildApp(){
+    $data = $this->getUp();
+    if(is_string($this->title) && is_string($this->script)){
+      $data['settings']['title'] = $this->title;
+      $data['settings']['script'] = $this->script;
+      $data['settings']['path'] = $this->path;
+      if($this->postUp($data)){
+        return true;
+      }
+    }
+    return false;
+  }
+  private function postUp($data){
+    $json = json_encode($data);
+    $fp = fopen($this->$)
+    if($fp){
+      if(flock($fp, LOCK_SH)){
+        if(fwrite($fp, $json)){
+          if(flock($fp, LOCK_UN)){
+            if(fclose($fp)){
+              return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+  private function getUp(){
+    return json_decode(file_get_contents($this->path), true);
   }
 }
 ?>
